e71051c5ccd91381afb78c90bd84777d03b00e15
TanyaVielkova <106912349+TetianaVielkova@users.noreply.github.com>
2023-04-26 18:22:09 +0300

    register-part1

---
+++ src/components/ValidationUserYup/ValidationUserYup.jsx
@@ -0,0 +1,7 @@
+import * as Yup from "yup";
+
+export const registerUserSchema = Yup.object().shape({
+    name: Yup.string().min(3, 'Too Short!').max(36, 'Too Long!').required('Required'),
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string().min(6, 'Too Short!').max(16, 'Too Long!').required('Required'),
+});
