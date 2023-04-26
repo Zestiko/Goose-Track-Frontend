66b7b73bc23e3d52b9ca5307409ed857c70935de
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-04-26 12:06:35 +0300

    userSlice ending

--- src/components/AccountPage/user-operations.js
+++ src/components/AccountPage/user-operations.js
@@ -1,30 +1,36 @@
 import axios from 'axios';
-import {
-  fetchInProgress,
-  fetchSuccess,
-  fetchError,
-} from "./userSlice";
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://goose-track-backend.herokuapp.com';
 
-export const fetchUser = () => async dispatch => {
+export const fetchUser = createAsyncThunk(
+  'user/info',
+  async (_, { rejectWithValue }) => {
     try {
-        dispatch(fetchInProgress());
-        const response = await axios.get("/user/current")
-        dispatch(fetchSuccess(response.data))
-    } catch (e) {
-        dispatch(fetchError(e.message))
- }
-};
-
-export const updateUserProfile = (formData) => async (dispatch) => {
-  try {
-    dispatch(fetchInProgress());
-
-    const response = await axios.patch("/user/info", formData);
-
-    dispatch(fetchSuccess(response.data));
-  } catch (error) {
-    dispatch(fetchError(error.message));
+      const { data } = await axios.get('/user');
+      return data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response.status === 401
+          ? 'Whoops, authorization required :('
+          : `Whoops, ${error.response.statusText.toLowerCase()}`
+      );
+    }
   }
-};
+)
+  
+export const updateUserProfile = createAsyncThunk(
+  'user/info',
+  async (formData, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.patch('/user/info', formData);
+      return data;
+    } catch (error) {
+      return rejectWithValue(
+        `Whoops, ${error.response.statusText.toLowerCase()} (${
+          error.response.status
+        })`
+      );
+    }
+  }
+);
