da57cd06c5fbf7825844087f609d1d73d072a1d9
IvanKoshel <educationZak@gmail.com>
2023-04-26 16:26:50 +0300

    SVG not showing up

--- src/components/MainPage/Sidebar/Navigation/Navigation.jsx
+++ src/components/MainPage/Sidebar/Navigation/Navigation.jsx
@@ -1,6 +1,12 @@
 import { useDispatch } from 'react-redux';
 import { NavLink, useLocation } from 'react-router-dom';
+import classNames from 'classnames';
+import userCheckSvg from '../../../../images/icons/user-check-01.svg';
+import calendarCheckSvg from '../../../../images/icons/calendar-check.svg';
 
+
+import css from './Navigation.module.scss';
+console.log(userCheckSvg);
 // Для теста
 const isActive = true;
 const getActiveClassName = ({ isActive }) =>
@@ -12,17 +18,33 @@ const Navigation = () => {
 
   // const isActiveToken
   return (
-      <div className="userPanel">
-        {/* {isActiveToken && <h2 className="">Please SignIn</h2>} */}
-        <p>User Panel</p>
-        <NavLink to="" className={getActiveClassName}>
-          My account
-        </NavLink>
-        <NavLink to="posts" end className={getActiveClassName}>
-          Calendar
-        </NavLink>
-        <button>Log out </button>
-      </div>
+    <div className="userPanel">
+      {/* {isActiveToken && <h2 className="">Please SignIn</h2>} */}
+      <p className={css.navLabel}>User Panel</p>
+      <NavLink
+        to="/myAccount"
+        className={classNames(
+          { getActiveClassName },
+          `${css.navItem}`,
+          `${css.navItem_1}`
+        )}
+      >
+        <svg>
+          <use href={userCheckSvg}></use>
+        </svg>
+        My account
+      </NavLink>
+      <NavLink
+        to="/calendar"
+        className={classNames({ getActiveClassName }, `${css.navItem}`)}
+      >
+        <svg>
+          <use href={calendarCheckSvg}></use>
+        </svg>
+        Calendar
+      </NavLink>
+      <button>Log out </button>
+    </div>
   );
 };
 
