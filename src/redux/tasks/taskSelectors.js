9ba43ab199b0243148ca4db85650e28c819882d7
Oleksandr P <goodwin009@gmail.com>
2023-04-27 12:20:28 +0300

    add Selectors && fix scss ChooseDay component

--- src/redux/tasks/taskSelectors.js
+++ src/redux/tasks/taskSelectors.js
@@ -1,5 +1,16 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectTasks = state => state.tasks.tasks;
 export const selectIsLoading = state => state.tasks.tasks;
 export const selectError = state => state.tasks.error;
 
-export const selectCoosedDate = state => state.choosedDate.value;
+export const selectChoosedDate = state => state.choosedDate.value;
+
+export const selectTasksByDay = day =>
+  createSelector(selectTasks, tasks =>
+    tasks.filter(task => {
+      const taskDate = new Date(task.taskDate).toLocaleDateString();
+      const userSelectedDay = new Date(day).toLocaleDateString();
+      return taskDate === userSelectedDay;
+    })
+  );
