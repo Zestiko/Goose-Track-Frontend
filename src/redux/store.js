1e0676ee6aac981547c1387e3fccde87a9387180
Oleksandr <105644022+BlackyHat@users.noreply.github.com>
2023-04-27 12:39:09 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,59 +1,34 @@
-import { addTask, fetchTasks, removeTask, updateTask } from "./tasksOperations";
-const { createSlice } = require("@reduxjs/toolkit");
 
-const initialState = {
-    tasks:[], 
-    isLoading: false, 
-    error:null
+import { configureStore } from '@reduxjs/toolkit';
+import { authInitState } from './auth/auth.init-state';
+import { authReducer } from './auth/authSlice';
+import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+
+const initState = {
+  auth: authInitState,
 };
 
-const handlePending = (state) => {
-    state.isLoading = true;
-}
+export const store = configureStore({
+  preloadedState: initState,
+  devTools: true,
+  reducer: {
+    auth: authReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
-const handleRejected = (state, action) => {
-    state.isLoading = false;
-    state.error = action.payload;
-}
+export const persistor = persistStore(store);
 
-const tasksSlice = createSlice({
-    name: 'tasks',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => 
-    builder
-    .addCase(fetchTasks.pending, handlePending)
-    .addCase(addTask.pending, handlePending)
-    .addCase(updateTask.pending, handlePending)
-    .addCase(removeTask.pending, handlePending)
-    .addCase(fetchTasks.rejected, handleRejected)
-    .addCase(addTask.rejected, handleRejected)
-    .addCase(updateTask.rejected, handleRejected)
-    .addCase(removeTask.rejected, handleRejected)
-    .addCase(fetchTasks.fullfilled, (state, action)=>{
-        state.isLoading = false;
-        state.error = null;
-        state.tasks = action.payload;
-    })
-    .addCase(addTask.fullfilled, (state, action)=>{
-        state.isLoading = false;
-        state.error = null;
-        state.tasks.push(action.payload);
+import { configureStore } from "@reduxjs/toolkit";
+import { userReducer } from "./user/userSlice";
 
-    })
-    .addCase(updateTask.fullfilled, (state, action)=>{
-        state.isLoading = false;
-        state.error = null;
-        const idx = state.tasks.findIndex((task)=> task.id === action.payload.id)
-        state.tasks.splice(idx,1,action.payload);
-    })
-    .addCase(removeTask.fullfilled, (state, action)=>{
-        state.isLoading = false;
-        state.error = null;
-        const idx = state.tasks.findIndex((task)=> task.id === action.payload.id)
-        state.tasks.splice(idx,1);
-
-    })
-})
-
-export const tasksReducer = tasksSlice.reducer;
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
