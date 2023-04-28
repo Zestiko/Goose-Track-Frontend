e5653b7860e1d73c012cca3ca71a772bb88a2044
Oleksandr P <goodwin009@gmail.com>
2023-04-28 18:28:53 +0300

    add link to mainPage && fix styles

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,31 +1,24 @@
-import scss  from './NotFoundPage.module.scss'
-
+import { Link } from 'react-router-dom';
+import scss from './NotFoundPage.module.scss';
 
 const NotFoundPage = () => {
+  return (
+    <section className={scss.pageBox}>
+      <div className={scss.wrapper}>
+        <h1 className={`${scss.sectionTitle}`}>404</h1>
+      </div>
 
-    return (<section className={scss.page_404}>
-	<div >
-		<div>	
-            <div >
-                <div >
-                    <div className={scss.four_zero_four_bg}>
-                        <h1 className={`${scss.text} `}>404</h1>
-                        </div>
-		
-		<div className={scss.contant_box_404}>
-		<h3 className={scss.info}>
-		Look like you're lost
-		</h3>
-		
-		<p  className={scss.info2}>the page you are looking for not avaible!</p>
-		
-		<a href="/" className={scss.button}>Go to Home</a>
-	</div>
-		</div>
-		</div>
-		</div>
-	</div>
-</section>)
-}
+      <div className={scss.infoInner}>
+        <h2 className={scss.infoTitle}>Look like you're lost</h2>
+        <p className={scss.infoMeta}>
+          the page you are looking for not avaible!
+        </p>
+        <Link to="/" className={scss.returnButton}>
+          Go to Home
+        </Link>
+      </div>
+    </section>
+  );
+};
 
 export default NotFoundPage;
