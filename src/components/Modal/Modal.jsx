b8190506661256aa640db321eadca58276280b40
Oleksandr P <goodwin009@gmail.com>
2023-04-29 09:58:44 +0300

    fix paths

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,28 +1,27 @@
-import scss from './BasicModal.module.scss'
+import scss from './Modal.module.scss';
 import { useEffect } from 'react';
-import {createPortal} from 'react-dom';
-import closeIcon from '../../images/icon-x-close.svg'
+import { createPortal } from 'react-dom';
+import closeIcon from '../../images/icons/icon-x-close.svg';
 
 /**
- * 
+ *
  * @parentElementComponents
  * import { useToggle } from 'hooks/useToggle';
  *  const {isOpen, onOpen,onClose} = useToggle();
- * 
+ *
  * Example
  * <button type='button' onClick={onOpen}>Open Modal</button>
- * 
-            * { isOpen && 
-            * <BasicModal onClose={onClose}>
-            * <h2>Modal title</h2>
-            * <p>Modal content goes here</p>
-            * <button type='submit' onClick={onClose}>DONE</button>
-            * </BasicModal>}
+ *
+ * { isOpen &&
+ * <BasicModal onClose={onClose}>
+ * <h2>Modal title</h2>
+ * <p>Modal content goes here</p>
+ * <button type='submit' onClick={onClose}>DONE</button>
+ * </BasicModal>}
  */
 
-const Modal = ({onClose, children}) => {
-
-useEffect(() => {
+const Modal = ({ onClose, children }) => {
+  useEffect(() => {
     const handleKeyDown = e => {
       if (e.key === 'Escape') {
         onClose && onClose();
@@ -36,26 +35,27 @@ useEffect(() => {
 
   const handleClickBackdrop = e => {
     if (e.target === e.currentTarget) {
-        onClose && onClose();
+      onClose && onClose();
     }
   };
   const handleClose = e => {
     if (e.target === e.currentTarget) {
-        onClose && onClose();
+      onClose && onClose();
     }
   };
 
-return (
-    createPortal(<div className={scss.modalBox}>
-        <div className={scss.modalOverlay} onClick={handleClickBackdrop}></div>
-        <div className={scss.modalBody}>
-            <button onClick={handleClose} className={scss.closeButton}> 
-                <img src={closeIcon} alt="1" className={scss.closeButtonIcon} />
-            </button>
-           {children}
-        </div>
-    </div>,document.getElementById('portal'))
-)
-}
+  return createPortal(
+    <div className={scss.modalBox}>
+      <div className={scss.modalOverlay} onClick={handleClickBackdrop}></div>
+      <div className={scss.modalBody}>
+        <button onClick={handleClose} className={scss.closeButton}>
+          <img src={closeIcon} alt="1" className={scss.closeButtonIcon} />
+        </button>
+        {children}
+      </div>
+    </div>,
+    document.getElementById('portal')
+  );
+};
 
 export default Modal;
