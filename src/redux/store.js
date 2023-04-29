6794ca9f976107304bf984acb5423658d8037c4f
Alieksieiev Vladyslav <kat9.hora@gmail.com>
2023-04-29 13:40:15 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,7 @@ import { authInitState } from './auth/auth.init-state';
 import { authReducer } from './auth/authSlice';
 import { persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import { userReducer } from "./user/userSlice";
+import { tasksReducer } from './tasks/taskSlice';
 
 const initState = {
   auth: authInitState,
@@ -15,6 +16,8 @@ export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    tasks: tasksReducer,
+    
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
