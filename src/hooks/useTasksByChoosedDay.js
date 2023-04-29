a13154447457c57dfbd3a725e2e858a06e8b7b31
Oleksandr P <goodwin009@gmail.com>
2023-04-29 15:04:57 +0300

    updateSelectors

--- src/hooks/useTasksByChoosedDay.js
+++ src/hooks/useTasksByChoosedDay.js
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { selectTasksByDay } from 'redux/tasks/tasks-selectors';
+import { selectTasksByDay } from 'redux/tasks/taskSelectors';
 
 export const useTasksByChoosedDay = currentDay => {
   const tasksByCurrentDay = selectTasksByDay(currentDay);
