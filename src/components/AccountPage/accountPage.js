958a548e9eaa01face77e790d3fdef3b5eba2792
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-04-30 21:36:36 +0300

    commit

--- src/components/AccountPage/accountPage.js
+++ src/components/AccountPage/accountPage.js
@@ -1,22 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectorGetUser } from '../../redux/user/selectors';
 import {
   authCurrentThunk,
   updateUserProfile,
+  uploadAvatar
 } from '../../redux/user/user-operations';
 import userAvatar from '../../images/icons/ph_user.svg';
+import { Formik, Form, Field } from 'formik';
+
 
 const UserForm = () => {
   const dispatch = useDispatch();
   const [previewImageUrl, setPreviewImageUrl] = useState(null);
   const { userName, email, birthday, phone, telegram, avatar } =
     useSelector(selectorGetUser);
+  const valuesRef = useRef({ userName, email, birthday, phone, telegram, avatar });
+  const dispatchRef = useRef(dispatch);
 
 
 
-  const handleAvatarChange = e => {
-    e.preventDefault();
+  const handleAvatarChange = async e => {
+    /*e.preventDefault();
     const file = e.target.files[0];
     if (!file) return;
 
@@ -25,28 +30,47 @@ const UserForm = () => {
 
     const imageUrl = window.URL.createObjectURL(file);
     console.log(imageUrl);
+    setPreviewImageUrl(imageUrl);*/
+  e.preventDefault();
+  const file = e.target.files[0];
+  if (!file) return;
+
+  try {
+    const imageUrl = await uploadAvatar(file);
+    console.log(imageUrl);
     setPreviewImageUrl(imageUrl);
+    valuesRef.current = { ...valuesRef.current, avatar: imageUrl };
+  } catch (error) {
+    console.error(error);
+    }
+  
   };
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  const handleSubmit = async (values) => {
+    const { userName, email, birthday, phone, telegram, avatar } = valuesRef.current;
+    
+    const formattedBirthday = birthday.replace(/(\d{4})-(\d{2})-(\d{2})/, '$3/$2/$1');
     const formData = new FormData();
     formData.append('userName', userName);
     formData.append('email', email);
     formData.append('birthday', birthday);
     formData.append('phone', phone);
     formData.append('telegram', telegram);
-    dispatch(updateUserProfile({formData}));
-    
-  };
-   
+    formData.append('avatar', avatar);
+    console.log(values)
+    try {
+      await dispatchRef.current(updateUserProfile(values));
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
-    <section className="user-page">
+   <section className="user-page">
       <div className="user-page__avatar-container">
         <img
           className="user-page__avatar"
-          src={avatar || previewImageUrl || userAvatar}
+          src={ avatar || previewImageUrl || userAvatar}
           alt="User Avatar"
         />
         <div className="avatar-upload-container">
@@ -62,61 +86,75 @@ const UserForm = () => {
         <h3 className="user-page__name">{userName || 'Username'}</h3>
         <p className="user-page__role">User</p>
       </div>
-      <form onSubmit={handleSubmit} className="username_form">
-        <label htmlFor="name" className="username_form-label">
-          Username
-        </label>
-        <input
-          className="username_form-input"
-          id="userName"
-          type="text"
-          value={userName}
-          placeholder="Enter your name"
-        />
-        <label htmlFor="birthday" className="username_form-label">
-          Birthday:
-        </label>
-        <input
-          className="username_form-input"
-          id="birthday"
-          type="date"
-          value={birthday}
-          placeholder="Enter your birthday"
-        />
-        <label htmlFor="email" className="username_form-label">
-          Email
-        </label>
-        <input
-          className="username_form-input"
-          id="email"
-          type="email"
-          value={email}
-          placeholder="Enter your email"
-        />
-        <label htmlFor="phone" className="username_form-label">
-          Phone:
-        </label>
-        <input
-          className="username_form-input"
-          id="phone"
-          type="text"
-          value={phone}
-          placeholder="Enter your phone"
-        />
-        <label htmlFor="telegram" className="username_form-label">
-          Telegram:
-        </label>
-        <input
-          className="username_form-input username_form-input--last"
-          id="telegram"
-          type="text"
-          value={telegram}
-          placeholder="Enter your telegram username"
-        />
-        <button type="submit" className="username__form-submit">
-          Save
-        </button>
-      </form>
+      <Formik
+        initialValues={{
+          userName: userName,
+          email: email,
+          birthday: birthday,
+          phone: phone,
+          telegram: telegram,
+        }}
+        onSubmit={handleSubmit}
+      >
+        {formik => (
+          <Form className="username_form">
+            <label htmlFor="userName" className="username_form-label">
+              Username
+            </label>
+            <Field
+              className="username_form-input"
+              id="userName"
+              name="userName"
+              type="text"
+              placeholder="Enter your name" 
+            />
+            <label htmlFor="birthday" className="username_form-label">
+              Birthday:
+            </label>
+            <Field
+              className="username_form-input"
+              id="birthday"
+              name="birthday"
+              type="date"
+              lang="en"
+              placeholder="Enter your birthday"
+            />
+            <label htmlFor="email" className="username_form-label">
+              Email
+            </label>
+            <Field
+              className="username_form-input"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Enter your email"
+            />
+            <label htmlFor="phone" className="username_form-label">
+              Phone:
+            </label>
+            <Field
+              className="username_form-input"
+              id="phone"
+              name="phone"
+              type="text"
+              placeholder="Enter your phone"
+            />
+            <label htmlFor="telegram" className="username_form-label">
+              Telegram:
+            </label>
+            <Field
+              className="username_form-input username_form-input--last"
+              id="telegram"
+              name="telegram"
+              type="text"
+              placeholder="Enter your Telegram link"
+            />
+            <button type="submit" className="username__form-submit">
+            Save
+            </button>
+          </Form>
+          )}
+      </Formik>
     </section>
   );
 };
