02675714fcbef432eced3558aa24c0c82cf1ac68
Pablo-Lipchanskyi <pablolipchansky@gmail.com>
2023-05-01 12:52:47 +0300

    commit

--- src/components/AccountPage/accountPage.js
+++ src/components/AccountPage/accountPage.js
@@ -1,8 +1,7 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectorGetUser } from '../../redux/user/selectors';
 import {
-  authCurrentThunk,
   updateUserProfile,
   uploadAvatar,
 } from '../../redux/user/user-operations';
@@ -11,65 +10,63 @@ import { Formik, Form, Field } from 'formik';
 
 const UserForm = () => {
   const dispatch = useDispatch();
-  const [previewImageUrl, setPreviewImageUrl] = useState(null);
+  
   const { userName, email, birthday, phone, telegram, avatar } =
     useSelector(selectorGetUser);
-  const valuesRef = useRef({
-    userName,
-    email,
-    birthday,
-    phone,
-    telegram,
-    avatar,
-  });
-  const dispatchRef = useRef(dispatch);
+  const [localUser, setLocalUser] = useState("")
+  const [localEmail, setLocalEmail] = useState("")
+  const [localBirthDay, setlocalBirthDay] = useState("")
+  const [localPhone, setLocalPhone] = useState("")
+  const [localTelegram, setLocalTelegram] = useState("")
+  const [previewImageUrl, setPreviewImageUrl] = useState("");
+  const [isImageSaved, setIsImageSaved] = useState(false);
 
   const handleAvatarChange = async e => {
-    e.preventDefault();
+    /*e.preventDefault();
     const file = e.target.files[0];
     if (!file) return;
 
     const formData = new FormData();
     formData.append('avatar', file);
-
-
-
-    await dispatchRef.current(updateUserProfile(formData));
     const imageUrl = window.URL.createObjectURL(file);
     console.log(imageUrl);
-    setPreviewImageUrl(imageUrl);
-    // e.preventDefault();
-    // const file = e.target.files[0];
-    // if (!file) return;
+    setPreviewImageUrl(imageUrl);*/
+     e.preventDefault();
+     const file = e.target.files[0];
+     if (!file) return;
 
-    // try {
-    //   const imageUrl = await uploadAvatar(file);
-    //   console.log(imageUrl);
-    //   setPreviewImageUrl(imageUrl);
-    //   valuesRef.current = { ...valuesRef.current, avatar: imageUrl };
-    // } catch (error) {
-    //   console.error(error);
-    //   }
+     try {
+      const imageUrl = await uploadAvatar(file);
+       setPreviewImageUrl(imageUrl);
+       setIsImageSaved(true);
+     } catch (error) {
+       console.error(error);
+       }
   };
 
   const handleSubmit = async values => {
-    const { userName, email, birthday, phone, telegram, avatar } =
-      valuesRef.current;
-
     // const formattedBirthday = birthday.replace(
     //   /(\d{4})-(\d{2})-(\d{2})/,
     //   '$3/$2/$1'
     // );
+    const response = await fetch(previewImageUrl);
+  const blob = await response.blob();
     const formData = new FormData();
-    formData.append('userName', userName);
-    formData.append('email', email);
-    formData.append('birthday', birthday);
-    formData.append('phone', phone);
-    formData.append('telegram', telegram);
+     if (previewImageUrl) {
+    formData.append('avatar', blob);
+  } else {
     formData.append('avatar', avatar);
-    console.log(values.avatar);
+    }
+    console.log(previewImageUrl)
+    formData.append('userName', localUser || userName);
+    formData.append('email', localEmail || email);
+    formData.append('birthday', localBirthDay || birthday);
+    formData.append('phone', localPhone || phone);
+    formData.append('telegram', localTelegram || telegram);
+    
+    console.log(values)
     try {
-      await dispatchRef.current(updateUserProfile(values));
+      await dispatch(updateUserProfile(formData));
     } catch (error) {
       console.log(error);
     }
@@ -80,7 +77,7 @@ const UserForm = () => {
       <div className="user-page__avatar-container">
         <img
           className="user-page__avatar"
-          src={avatar || previewImageUrl || userAvatar}
+          src={previewImageUrl || avatar || userAvatar}
           alt="User Avatar"
         />
         <div className="avatar-upload-container">
@@ -113,63 +110,72 @@ const UserForm = () => {
           <Form className="username_form">
             <label htmlFor="userName" className="username_form-label">
               Username
-            </label>
-            <Field
+               <Field
               className="username_form-input"
               id="userName"
               name="userName"
               type="text"
               placeholder="Enter your name"
-              value={userName}
-            />
+              onChange={(e) => setLocalUser(e.target.value)}
+              value={localUser || userName}
+                />
+              </label>
+
             <label htmlFor="birthday" className="username_form-label">
               Birthday:
-            </label>
-            <Field
+              <Field
               className="username_form-input"
               id="birthday"
               name="birthday"
               type="date"
               lang="en"
               placeholder="Enter your birthday"
-              value={birthday.slice(0,9) && `2001-11-11`}
+              onChange={(e) => setlocalBirthDay(e.target.value)}
+              value={localBirthDay || birthday.slice(0,10) || `2001-11-11`}
             />
+              </label>
+
+
             <label htmlFor="email" className="username_form-label">
               Email
-            </label>
-            <Field
+              <Field
               className="username_form-input"
               id="email"
               name="email"
               type="email"
-              value={email}
+              onChange={(e)=> setLocalEmail(e.target.value)}
+              value={localEmail || email}
               placeholder="Enter your email"
             />
+              </label>
+
             <label htmlFor="phone" className="username_form-label">
               Phone:
-            </label>
-            <Field
+             <Field
               className="username_form-input"
               id="phone"
               name="phone"
               type="text"
               placeholder="Enter your phone"
-              value={phone}
+              onChange={(e)=> setLocalPhone(e.target.value)}
+              value={localPhone || phone}
             />
+              </label>
             <label htmlFor="telegram" className="username_form-label">
               Telegram:
-            </label>
-            <Field
+              <Field
               className="username_form-input username_form-input--last"
               id="telegram"
               name="telegram"
               type="text"
               placeholder="Enter your Telegram link"
-              value={telegram}
+              onChange={(e)=> setLocalTelegram(e.target.value)}
+              value={localTelegram || telegram}
             />
+              </label>
             <button type="submit" className="username__form-submit">
               Save
-            </button>
+              </button>
           </Form>
         )}
       </Formik>
