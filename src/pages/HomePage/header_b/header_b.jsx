f67e335b8020bbc59f21b581ea4113ee47180277
Mastermind2025 <85400723+Mastermind2025@users.noreply.github.com>
2023-05-02 23:24:23 +0300

    st

--- src/pages/HomePage/header_b/header_b.jsx
+++ src/pages/HomePage/header_b/header_b.jsx
@@ -2,6 +2,10 @@ import styles from './header.module.scss';
 import goose from '../assets/img/GOOSE_2.png';
 import svgIcon from '../assets/img/Icon_arr.svg';
 import { NavLink } from 'react-router-dom';
+// Goose
+import gooseDesk2x from '../../../images/bar/desktop/goose-1-desktop-2x.png';
+import gooseMob2x from '../../../images/bar/mobile/goose-1-mobile-2x.png';
+import gooseTab2x from '../../../images/bar/tablet/goose-1-tablet-2x.png';
 
 let handleDel;
 
@@ -9,7 +13,17 @@ export const Header = () => {
   return (
     <div className={styles.container}>
       <div className={styles.box}>
-        <img src={goose} alt="logo" className={styles.logo} />
+        {/* <img src={goose} alt="logo" className={styles.logo} /> */}
+
+<picture>
+<source srcset={gooseDesk2x} media="(min-width: 1200px)" type="image/jpg"/>
+<source srcset={gooseTab2x} media="(min-width: 768px)" type="image/jpg"/>
+<source srcset={gooseMob2x} media="(min-width: 450px)" type="image/jpg"/>
+
+<img class="our__peoplepic" src={goose} alt="Goose"/>
+</picture>     
+
+
 
         <h1 className={styles.main_title}>
           G<span className={styles.oo_title}>oo</span>seTrack
