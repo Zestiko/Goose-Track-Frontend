14a18ab812084cae7205c92aca5dc6c19b701bb0
Oleksandr P <goodwin009@gmail.com>
2023-05-02 18:35:05 +0300

    add useEffect to load tasks for ChoosedDay

--- src/components/сalendar/calendarDaysTask/CalendarDaysTask.jsx
+++ src/components/сalendar/calendarDaysTask/CalendarDaysTask.jsx
@@ -1,21 +1,26 @@
+import css from '../Calendar.module.css';
+import { CalendarHeaderWeek } from '../calendarHeaderWeek/CalendarHeaderWeek';
+import { CalendarHeaderTask } from './сalendarHeaderTask/CalendarHeaderTask';
 
-import css from "../Calendar.module.css";
-import { CalendarHeaderWeek } from "../calendarHeaderWeek/CalendarHeaderWeek";
-import { CalendarHeaderTask } from "./сalendarHeaderTask/CalendarHeaderTask";
-
-import { getCurrentDate } from "../../../redux/calendar/selectors";
-import TasksColumnsList from "components/TasksColumnsList/TasksColumnsList";
-import { useSelector } from "react-redux";
+import { getCurrentDate } from '../../../redux/calendar/selectors';
+import TasksColumnsList from 'components/TasksColumnsList/TasksColumnsList';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchTasks } from 'redux/tasks/tasksOperations';
 
 const CalendarDaysTask = () => {
-    const currentDate = useSelector(getCurrentDate);
-    return (
-         <div className={css.calendar_wrapper}>
-            <CalendarHeaderTask/>
-            <CalendarHeaderWeek/>
-            <TasksColumnsList selectedDay={currentDate} />
-        </div>
-        
-    )
-}
+  const currentDate = useSelector(getCurrentDate);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchTasks(currentDate));
+  }, [currentDate, dispatch]);
+
+  return (
+    <div className={css.calendar_wrapper}>
+      <CalendarHeaderTask />
+      <CalendarHeaderWeek />
+      <TasksColumnsList selectedDay={currentDate} />
+    </div>
+  );
+};
 export default CalendarDaysTask;
