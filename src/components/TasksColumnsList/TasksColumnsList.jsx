af412e5cf7047c089acd5b92643580509e54f53b
Maryna Vyshnevska <vyshnevska.maryna@gmail.com>
2023-05-03 14:19:18 +0300

    Color

--- src/components/TasksColumnsList/TasksColumnsList.jsx
+++ src/components/TasksColumnsList/TasksColumnsList.jsx
@@ -1,7 +1,6 @@
 import TasksColumn from 'components/ChoosedDay/TasksColumn/TasksColumn';
 import scss from './TasksColumnsList.module.scss';
-
-const COLUMNS = ['toDo', 'inProgress', 'done'];
+import { COLUMNS_OPTIONS, COLUMNS } from 'constants/columns.constans';
 
 const TasksColumnsList = ({ dayTasks }) => {
   const tasksByColumns = COLUMNS.reduce((acc, column) => {
@@ -14,11 +13,14 @@ const TasksColumnsList = ({ dayTasks }) => {
   return (
     <div className={scss.listBox}>
       {COLUMNS.map(column => {
+        const columnOptions = COLUMNS_OPTIONS.find(option => option.column === column);
+        const title = columnOptions ? columnOptions.title : '';
+
         return (
           <TasksColumn
             key={column}
             tasks={tasksByColumns[column]}
-            title={column}
+            title={title}
             column={column}
           />
         );
