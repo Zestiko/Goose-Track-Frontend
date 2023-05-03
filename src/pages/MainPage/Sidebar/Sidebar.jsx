e1cb345971e1ab89ce317f69eaaf9a2743a2d3db
Mastermind2025 <85400723+Mastermind2025@users.noreply.github.com>
2023-05-03 18:48:55 +0300

    go on18:48

--- src/pages/MainPage/Sidebar/Sidebar.jsx
+++ src/pages/MainPage/Sidebar/Sidebar.jsx
@@ -2,12 +2,28 @@ import Navigation from './Navigation/Navigation';
 import logoImg from '../../../images/logoImg.png';
 import { ReactComponent as Xclose } from '../../../images/icons/x-close.svg';
 
+// Goose Retina
+import gooseDesk3x from '../../../images/bar/desktop/goose-1-desktop-3x.png';
+import gooseDesk2x from '../../../images/bar/desktop/goose-1-desktop-2x.png';
+import gooseDesk1x from '../../../images/bar/desktop/goose-1-desktop-1x.png';
+
+
 import css from './Sidebar.module.scss';
 
 const Sidebar = ({ isSidebarOpen, handleBurgerMenuClick, theme }) => (
   <div className={`${css.sidebar} ${isSidebarOpen ? css.open : ''} ${theme}`}>
     <div className={css.logo}>
-      <img className={css.logoImg} src={logoImg} alt="logoImage"></img>
+
+    <picture>
+        <source
+            
+            srcSet={`${gooseDesk1x} 1x, ${gooseDesk2x} 2x, ${gooseDesk3x} 3x`} 
+            alt="Goose"
+        />
+        <img className={css.logoImg} src={logoImg} alt="logoImage"/>       
+    </picture>    
+    {/* <img className={css.logoImg} src={logoImg} alt="logoImage"></img> */}
+      
       <p className={css.logoText}>
         G<i>oo</i>seTrack
       </p>
