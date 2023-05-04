da9013d67e9e8870fe125cde387a1480b985171c
Alieksieiev Vladyslav <kat9.hora@gmail.com>
2023-05-04 13:41:05 +0300

    Update Calendar.jsx

--- src/components/сalendar/Calendar.jsx
+++ src/components/сalendar/Calendar.jsx
@@ -1,31 +1,24 @@
-
-
-
-import css from "./Calendar.module.scss";
-import { useEffect } from "react";
-
-import { CalendarHeaderDays } from "./calendarHeaderDays/CalendarHeaderDays";
-import { CalendarGrid } from "./сalendarGrid/CalendarGrid";
-import { CalendarHeader } from "./сalendarHeader/CalendarHeader";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchTasks } from "redux/tasks/tasksOperations";
-import { getCurrentDate } from "../../redux/calendar/selectors";
+import { useEffect } from 'react';
+import { CalendarHeaderDays } from './calendarHeaderDays/CalendarHeaderDays';
+import { CalendarGrid } from './сalendarGrid/CalendarGrid';
+import { CalendarHeader } from './сalendarHeader/CalendarHeader';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTasks } from 'redux/tasks/tasksOperations';
+import { getCurrentDate } from '../../redux/calendar/selectors';
 
 const Calendar = () => {
   const currentDate = useSelector(getCurrentDate);
-      const dispatch = useDispatch();
+  const dispatch = useDispatch();
   useEffect(() => {
+    dispatch(fetchTasks(currentDate));
+  }, [currentDate, dispatch]);
 
-    dispatch(fetchTasks(currentDate))
-  },[currentDate, dispatch])
-
-    return (
-    <div className=''>
-        <CalendarHeader/>
-        <CalendarHeaderDays/>
-        <CalendarGrid/>
-      </div>
-
-  )
-}
+  return (
+    <div className="">
+      <CalendarHeader />
+      <CalendarHeaderDays />
+      <CalendarGrid />
+    </div>
+  );
+};
 export default Calendar;
