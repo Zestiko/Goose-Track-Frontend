0a7319db12855c6f74fa2962e21f8c9f9bd1db3a
IvanKoshel <educationZak@gmail.com>
2023-05-04 20:35:38 +0300

    Calendar css Complete

--- src/components/AccountPage/DatePicker/DatePicker.jsx
+++ src/components/AccountPage/DatePicker/DatePicker.jsx
@@ -57,6 +57,7 @@ const MyDatePicker = ({ name = '', birthday }) => {
       calendarStartDay={1}
       placeholderText={birthday || 'Choose a date'}
       formatWeekDay={formatWeekDay}
+      showPopperArrow={false}
     />
   );
 };
