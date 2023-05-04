a18a855614f8792fe597c438d83531f454336fcb
Alieksieiev Vladyslav <kat9.hora@gmail.com>
2023-05-04 20:56:52 +0300

    Fix

--- src/pages/HomePage/reviews/reviews.jsx
+++ src/pages/HomePage/reviews/reviews.jsx
@@ -19,45 +19,59 @@ const nextSlide = () => {
     setCurrent(newIndex >= data.length ? 0 : newIndex);}
 
 return (
-    <><div className={styles.container}>
-        <h2 className={styles.foot__title}>REVIEWS</h2>
-        <ul className={styles.list}>
-            {data.slice(
+  <>
+    <div className={styles.container}>
+      <h2 className={styles.foot__title}>REVIEWS</h2>
+      <ul className={styles.list}>
+        {data
+          .slice(
             current,
-            current + Math.min(data.length - current, ((window.innerWidth >= 1280) ? 2 : 1))
-        )
-        .map((review, index) => (
-                <li className={styles.item}>
-                    <div
-                        key={review.id}
-                    >
-                        <div className={styles.block}>
-                            <img src={review.avatar} alt={review.name} className={styles.photo} />
-                            <div className={styles.blockMob}>
-                                <h2 className={styles.name}>{review.name}</h2>
-                                <div className="rating">
-                                {[...Array(5)].map((_, i) => (
-                                    <span
-                                        key={i}
-                                        className={i < review.rating ? `${styles.starActive}` : `${styles.star}`}
-                                    >
-                                        <BsFillStarFill/>
-                                    </span>
-                                    ))}
-                                </div>
-                                <p className={styles.text}>{review.text}</p>
-                            </div>
-                        </div>
+            current +
+              Math.min(data.length - current, window.innerWidth >= 1280 ? 2 : 1)
+          )
+          .map((review, index) => (
+            <li className={styles.item} key={review.id}>
+              <div>
+                <div className={styles.block}>
+                  <img
+                    src={review.avatar}
+                    alt={review.name}
+                    className={styles.photo}
+                  />
+                  <div className={styles.blockMob}>
+                    <h2 className={styles.name}>{review.name}</h2>
+                    <div className="rating">
+                      {[...Array(5)].map((_, i) => (
+                        <span
+                          key={i}
+                          className={
+                            i < review.rating
+                              ? `${styles.starActive}`
+                              : `${styles.star}`
+                          }
+                        >
+                          <BsFillStarFill />
+                        </span>
+                      ))}
                     </div>
-                </li>))}
-        </ul>
-        <div className={styles.icons}>
-            <button className={styles.button} onClick={prevSlide}><img src={downArrow2} alt="Arrow" className={styles.icon}/></button>
-            <button className={styles.button} onClick={nextSlide}><img src={downArrow1} alt="Arrow" className={styles.icon}/></button>
-        </div>
+                    <p className={styles.text}>{review.text}</p>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+      </ul>
+      <div className={styles.icons}>
+        <button className={styles.button} onClick={prevSlide}>
+          <img src={downArrow2} alt="Arrow" className={styles.icon} />
+        </button>
+        <button className={styles.button} onClick={nextSlide}>
+          <img src={downArrow1} alt="Arrow" className={styles.icon} />
+        </button>
+      </div>
     </div>
-    </>
-    );
+  </>
+);
 };
 
 export default Slider;
